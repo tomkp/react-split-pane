9605946b7486574056c9e2ed9e9a5accda093e26
tomkp <tom@tomkp.com>
2015-05-28 21:14:54 +0100

    improving tests

--- test/SplitPane-test.js
+++ test/SplitPane-test.js
@@ -5,10 +5,66 @@ import SplitPane from '../src/SplitPane';
 import Resizer from '../src/Resizer';
 
 
-describe('SplitPane', function () {
+describe('Default SplitPane', function () {
+
+    const splitPane = TestUtils.renderIntoDocument(
+        <SplitPane>
+            <div>one</div>
+            <div>two</div>
+        </SplitPane>
+    );
+
+
+    it('should render the child panes', function () {
+        new Asserter(splitPane).assertPaneContents(['one', 'two']);
+    });
+
+
+    it('should have vertical orientation', function () {
+        new Asserter(splitPane).assertOrientation('vertical');
+    });
+
+
+    it('should contain a Resizer', function () {
+        new Asserter(splitPane).assertContainsResizer();
+    });
+
+});
+
+
+
+describe('Horizontal SplitPane', function () {
 
     const splitPane = TestUtils.renderIntoDocument(
         <SplitPane orientation="horizontal">
+            <div>one</div>
+            <div>two</div>
+        </SplitPane>
+    );
+
+
+    it('should render the child panes', function () {
+        new Asserter(splitPane).assertPaneContents(['one', 'two']);
+    });
+
+
+    it('should have horizontal orientation', function () {
+        new Asserter(splitPane).assertOrientation('horizontal');
+    });
+
+
+    it('should contain a Resizer', function () {
+        new Asserter(splitPane).assertContainsResizer();
+    });
+
+});
+
+
+
+describe('Vertical SplitPane', function () {
+
+    const splitPane = TestUtils.renderIntoDocument(
+        <SplitPane orientation="vertical">
             <div>one</div>
             <div>two</div>
         </SplitPane>
@@ -21,17 +77,49 @@ describe('SplitPane', function () {
     });
 
 
-    it('should have className of orientation', function () {
-        const component = TestUtils.findRenderedDOMComponentWithClass(splitPane, 'SplitPane');
-        expect(component.getDOMNode().className).to.contain('horizontal');
+    it('should have vertical orientation', function () {
+        new Asserter(splitPane).assertOrientation('vertical');
     });
 
 
     it('should contain a Resizer', function () {
-        const component = TestUtils.findRenderedDOMComponentWithClass(splitPane, 'SplitPane');
-        expect(component.props.children.length).to.equal(3);
-        const resizer = TestUtils.scryRenderedComponentsWithType(splitPane, Resizer);
-        expect(resizer.length).to.be(1);
+        new Asserter(splitPane).assertContainsResizer();
     });
 
 });
+
+
+
+
+
+class Asserter {
+
+    constructor(splitPane) {
+        this.component = TestUtils.findRenderedDOMComponentWithClass(splitPane, 'SplitPane');
+        this.splitPane = splitPane;
+    }
+
+
+    assertOrientation(expectedOrientation) {
+        expect(this.component.getDOMNode().className).to.contain(expectedOrientation);
+        return this;
+    }
+
+
+    assertPaneContents(expectedContents) {
+        let panes = TestUtils.scryRenderedDOMComponentsWithClass(this.component, 'Pane');
+        var values = panes.map((pane) => {
+            return pane.getDOMNode().textContent;
+        });
+        expect(values).to.eql(expectedContents);
+        return this;
+    }
+
+
+    assertContainsResizer(){
+        expect(this.component.props.children.length).to.equal(3);
+        const resizer = TestUtils.scryRenderedComponentsWithType(this.splitPane, Resizer);
+        expect(resizer.length).to.be(1);
+    }
+}
+
