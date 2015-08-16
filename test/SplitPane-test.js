a3c31bd4227f01bd0d3197e1e5ff94c05239879a
tomkp <tom@tomkp.com>
2015-08-16 20:45:29 +0100

    assert on defaultSize

--- test/SplitPane-test.js
+++ test/SplitPane-test.js
@@ -111,10 +111,10 @@ describe('Horizontal SplitPane', function () {
     });
 
 
-    describe('With default size', function () {
+    describe('With defaultSize property', function () {
 
         const splitPane = (
-            <SplitPane split="horizontal" defaultSize="100" >
+            <SplitPane split="horizontal" defaultSize="99" >
                 <div>one</div>
                 <div>two</div>
             </SplitPane>
@@ -147,7 +147,8 @@ describe('Horizontal SplitPane', function () {
                 position: 'relative',
                 outline: 'none',
                 overflow: 'auto',
-                WebkitFlex: '0 0 auto'
+                WebkitFlex: '0 0 auto',
+                height: '99px'
             });
         });
 
@@ -157,7 +158,8 @@ describe('Horizontal SplitPane', function () {
                 position: 'relative',
                 outline: 'none',
                 overflow: 'auto',
-                WebkitFlex: '1 1 0px'
+                WebkitFlex: '1 1 0px',
+                height: ''
             });
         });
 
@@ -253,5 +255,82 @@ describe('Vertical SplitPane', function () {
 
     });
 
+
+
+    describe('With defaultSize property', function () {
+
+        const splitPane = (
+            <SplitPane split="vertical" defaultSize="99" >
+                <div>one</div>
+                <div>two</div>
+            </SplitPane>
+        );
+
+
+        it('should render the SplitPane', function () {
+            new Asserter(splitPane).assertPaneContents(['one', 'two']);
+        });
+
+
+        it('should use correct css properties', function () {
+            new Asserter(splitPane).assertSplitPaneStyles({
+                display: '-webkit-flex',
+                height: '100%',
+                left: '0px',
+                position: 'absolute',
+                outline: 'none',
+                overflow: 'hidden',
+                right: '0px',
+                WebkitFlex: '1 1 0px',
+                WebkitFlexDirection: 'row',
+                WebkitUserSelect: 'none'
+            });
+        });
+
+
+        it('should use correct css properties for the left Pane', function () {
+            new Asserter(splitPane).assertFirstPaneStyles({
+                position: 'relative',
+                outline: 'none',
+                overflow: 'auto',
+                WebkitFlex: '0 0 auto',
+                width: '99px'
+            });
+        });
+
+
+        it('should use correct css properties for the right Pane', function () {
+            new Asserter(splitPane).assertSecondPaneStyles({
+                position: 'relative',
+                outline: 'none',
+                overflow: 'auto',
+                WebkitFlex: '1 1 0px',
+                width: ''
+            });
+        });
+
+
+        it('should render the child panes', function () {
+            new Asserter(splitPane).assertPaneContents(['one', 'two']);
+        });
+
+
+        it('should have vertical orientation', function () {
+            new Asserter(splitPane).assertOrientation('vertical');
+        });
+
+
+        it('should contain a Resizer', function () {
+            new Asserter(splitPane).assertContainsResizer();
+        });
+
+
+        it('should change size when resized', function () {
+            new Asserter(splitPane)
+                .selectResizer()
+            ;
+        });
+
+    });
 
 });
