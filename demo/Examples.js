6d232afd4e82735f92963f4723022659f6ff9688
tomkp <tom@tomkp.com>
2016-04-20 15:45:54 +0100

    update examples to es6 pure render components

--- demo/Examples.js
+++ demo/Examples.js
@@ -1,47 +1,39 @@
-import React from 'react';
-import {render} from 'react-dom';
-import SplitPane from '../lib/SplitPane';
+import React from "react";
+import {render} from "react-dom";
+import SplitPane from "../lib/SplitPane";
 
 
-var Example1 = React.createClass({
-    render: function() {
-        return (
-            <SplitPane split="vertical" minSize={50} maxSize={300} defaultSize={100} className="primary">
-                <div></div>
-                <SplitPane split="horizontal">
-                    <div></div>
-                    <div></div>
-                </SplitPane>
-            </SplitPane>
-        );
-    }
-});
-
-
-var Example2 = React.createClass({
-    render: function() {
-        return (
-            <SplitPane split="vertical">
-                <div></div>
-                <div></div>
-            </SplitPane>
-        );
-    }
-});
-
-var Example3 = React.createClass({
-    render: function() {
-        return (
+const SimpleNestedExample = () => {
+    return (
+        <SplitPane split="vertical" minSize={50} maxSize={300} defaultSize={100} className="primary">
+            <div></div>
             <SplitPane split="horizontal">
                 <div></div>
                 <div></div>
             </SplitPane>
-        );
-    }
-});
+        </SplitPane>
+    );
+};
+
+const BasicVerticalExample = () => {
+    return (
+        <SplitPane split="vertical">
+            <div></div>
+            <div></div>
+        </SplitPane>
+    );
+};
+
+const BasicHorizontalExample = () => {
+    return (
+        <SplitPane split="horizontal">
+            <div></div>
+            <div></div>
+        </SplitPane>
+    );
+};
 
 
-
-if (document.getElementById("example1")) render(<Example1 />, document.getElementById("example1"));
-if (document.getElementById("example2")) render(<Example2 />, document.getElementById("example2"));
-if (document.getElementById("example3")) render(<Example3 />, document.getElementById("example3"));
+if (document.getElementById("example1")) render(<SimpleNestedExample />, document.getElementById("example1"));
+if (document.getElementById("example2")) render(<BasicVerticalExample />, document.getElementById("example2"));
+if (document.getElementById("example3")) render(<BasicHorizontalExample />, document.getElementById("example3"));
