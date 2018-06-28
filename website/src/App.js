7d51521e5bb1b8dd43cd603b17deb28191050be9
Wei-Wei Wu <wuxx1045@umn.edu>
2018-06-28 02:00:13 -0700

    pretty sidebar

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import styled from 'react-emotion';
+import { BrowserRouter } from 'react-router-dom';
+
+import Sidebar from './components/Sidebar';
+
+const Wrapper = styled('div')`
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+`;
+
+const Container = styled('div')`
+  background-color: blue;
+  width: 100%;
+  height: 100%;
+`;
 
 export default function App() {
-  return <div> HI</div>;
+  return (
+    <BrowserRouter>
+      <Wrapper>
+        <Sidebar />
+        <Container />
+      </Wrapper>
+    </BrowserRouter>
+  );
 }
